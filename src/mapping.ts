faef772614621f2af89d2b2dcbf188c72617e663
0xIbuki <junta8918@gmail.com>
2022-09-20 22:16:45 +0900

    changed to use cosmoshub-ts, track Send message

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,38 +1,31 @@
-import { cosmos } from "@graphprotocol/graph-ts";
-import { MsgDelegate, MsgCoin, decodeMsgDelegate } from "./decoding";
-import { Delegation, Coin } from "../generated/schema";
+import { cosmos, BigInt } from "@graphprotocol/graph-ts";
+import { Send, Block } from "../generated/schema";
+import { cosmos as cosmoshub } from "@graphprotocol/cosmoshub-ts";
 
 export function handleTx(data: cosmos.TransactionData): void {
   const id = `${data.block.header.hash.toHexString()}-${data.tx.index}`;
   const messages = data.tx.tx.body.messages;
 
+  const block = new Block(id);
+  block.number = BigInt.fromString(data.block.header.height.toString());
+  block.timestamp = BigInt.fromString(
+    data.block.header.time.seconds.toString()
+  );
+  block.save();
+
   for (let i = 0; i < messages.length; i++) {
     let msgType = messages[i].typeUrl;
     let msgValue = messages[i].value as Uint8Array;
 
-    if (msgType == "/cosmos.staking.v1beta1.MsgDelegate") {
-      saveDelegation(id, decodeMsgDelegate(msgValue)) // The message needs to be decoded to access its attributes.
+    if (msgType == "/cosmos.bank.v1beta1.MsgSend") {
+      const message = cosmoshub.bank.v1beta1.decodeMsgSend(msgValue);
+      const msg = new Send(id);
+      msg.fromAddress = message.from_address;
+      msg.toAddress = message.to_address;
+      msg.amount = BigInt.fromString(message.amount[0].amount);
+      msg.hash = data.tx.hash;
+      msg.block = block.id;
+      msg.save();
     }
   }
-}
-
-function saveDelegation(id: string, message: MsgDelegate): void {
-  const msg = new Delegation(id);
-
-  msg.delegatorAddress = message.delegator_address;
-  msg.validatorAddress = message.validator_address;
-  msg.amount = saveCoin(id, message.amount as MsgCoin);
-
-  msg.save();
-}
-
-function saveCoin(id: string, c: MsgCoin): string {
-  const coin = new Coin(id);
-
-  coin.amount = c.amount;
-  coin.denom = c.denom;
-
-  coin.save();
-
-  return id;
 }
